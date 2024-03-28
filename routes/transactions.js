652e7fe8e83240a81340e854ea347a8ec348b052
swathimuneeswaran <swathirajan255@gmail.com>
2024-03-28 12:54:51 +0530

    change

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,19 +2,19 @@ const { addExpense, getExpense, deleteExpense } = require('../controllers/expens
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 const {resetPassword,forgotPassword,signIn,addUser,getAllUsers}=require('../controllers/user');
 const router = require('express').Router();
-const verifyUser=require("../controllers/verify")
 
 
-router.post('/add-income',verifyUser,addIncome)
-router.get('/get-incomes/:email',verifyUser, getIncomes)
-router.delete('/delete-income/:id', verifyUser,deleteIncome)
-router.post('/add-expense',verifyUser, addExpense)
-router.get('/get-expenses/:email',verifyUser, getExpense)
-router.delete('/delete-expense/:id', verifyUser,deleteExpense)
+
+router.post('/add-income',addIncome)
+router.get('/get-incomes/:email', getIncomes)
+router.delete('/delete-income/:id', deleteIncome)
+router.post('/add-expense', addExpense)
+router.get('/get-expenses/:email',getExpense)
+router.delete('/delete-expense/:id',deleteExpense)
 router.post("/signup",addUser);
 router.post("/login",signIn);
 router.post("/forgot-password",forgotPassword);
 router.post("/reset-password/:token",resetPassword)
-router.get("/users",verifyUser,getAllUsers)
+router.get("/users",getAllUsers)
 
 module.exports = router
